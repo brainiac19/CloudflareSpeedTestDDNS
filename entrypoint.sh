f1c75570a52a7e57b5bfcd02a4043abe0c1af0a3
Tim.P <51896856+brainiac19@users.noreply.github.com>
2024-06-05 23:21:12 +0800

    fix dcron for alpine

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,12 @@ COMMAND="/bin/bash /app/start.sh"
 if [ -z "$CRON" ]; then
     echo "CRON environment variable not set, running once..."
     $COMMAND
+else
+    TEMP_FILE=$(mktemp)
+    echo "$CRON $COMMAND" > "$TEMP_FILE"
+    crontab "$TEMP_FILE"
+    rm "$TEMP_FILE"
+    exec crond -f -l 2
 fi
 
 
-# Create a temporary file for the cron job
-TEMP_FILE=$(mktemp)
-
-# Add the cron schedule and command to the temp file
-echo "$CRON $COMMAND" > "$TEMP_FILE"
-
-# Install the new cron job from the temp file
-crontab "$TEMP_FILE"
-
-# Remove the temporary file
-rm "$TEMP_FILE"
-
-# Start the cron daemon in the foreground
-exec crond -f -l 2
