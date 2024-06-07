78671a011ee429120934fb23f6b50353aab56b37
Tim.P <51896856+brainiac19@users.noreply.github.com>
2024-06-07 16:26:11 +0800

    minor fix

--- cf_token_check.sh
+++ cf_token_check.sh
@@ -2,7 +2,7 @@
 config_file="./volume/config.conf"
 source "$config_file"
 
-res=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}" -H "Authorization: Bearer $api_key" -H "Content-Type:application/json");
+res=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}" -H "Authorization: Bearer $CF_API_KEY" -H "Content-Type:application/json");
 resSuccess=$(echo "$res" | jq -r ".success");
 if [[ $resSuccess != "true" ]]; then
   echo "登陆错误，检查cloudflare账号信息填写是否正确!"
